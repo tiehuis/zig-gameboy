17e6284c40aa41ef0687bc4c37ea8550951a98b6
Marc Tiehuis <marctiehuis@gmail.com>
2017-06-10 15:55:05 +1200

    Add build file

---
+++ build.zig
@@ -0,0 +1,16 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: &Builder) {
+    const mode = b.standardReleaseOptions();
+
+    var exe = b.addExecutable("gameboy", "src/main.zig");
+    exe.setBuildMode(mode);
+
+    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
+
+    const play = b.step("run", "Run the emulator");
+    const run = b.addCommand(".", b.env_map, exe.getOutputPath(), [][]const u8{});
+    play.dependOn(&run.step);
+    run.step.dependOn(&exe.step);
+}
